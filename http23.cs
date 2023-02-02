e2c32eb199b257e3f5fced4033d122298197b936
He11oW-10 <46309857+He11oW-10@users.noreply.github.com>
2023-02-02 15:03:23 +0800

    Update http23.cs

--- http23.cs
+++ http23.cs
@@ -14,14 +14,14 @@ namespace netfunc
     public static class http23
     {
 [FunctionName("TimerTriggerCSharp")]
-public static void Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log)
+public static void Run([TimerTrigger("0 */2 * * * *")]TimerInfo myTimer, ILogger log, UseMonitor=False)
 {
     if (myTimer.IsPastDue)
     {
         log.LogInformation("Timer is running late!");
     }
     Thread.Sleep(180000);
-    log.LogInformation(ScheduleStatus.Next);
+    log.LogInformation(myTimer.ScheduleStatus.Next);
     log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 }
     }
